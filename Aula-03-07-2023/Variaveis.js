e47af57e225cb2af248e6d8ea9501cc3d545c37d
Coder_Kanon <carloseduardomendes201@gmail.com>
2023-07-03 12:02:27 -0300

    Variaveis

---
+++ Aula-03-07-2023/Variaveis.js
@@ -0,0 +1,51 @@
+// JavaScrip - Linguagem de Programação (Interpretada)
+// Java - linguagem de Programação (Compilada)
+
+/*
+    Comentários de multiplas linhas
+*/ 
+// Comentário de unica linha
+
+// Chrome - V8 - Interpretador do jS
+// Node.js - v8 + npm (Node Package Manager)
+
+// console.log("Texto") // imprime bo terminal/console uma informação
+// console.log(5)
+
+
+
+// Variaveis - Espaço (na memória) para armazenar um valor
+let nome; // declaração da variavel nome
+nome = "Carlos"; // atribuição do valor a variavel
+console.log(nome);
+
+let idade;
+idade = 23; // declaração da variavel e atribuição do valor
+console.log(idade);
+
+let altura;
+altura = 1.78;
+console.log(altura);
+
+let cpf = "123.456.789-00";
+console.log(cpf);
+
+let casado = false; // false e true
+console.log(casado);
+
+// substitui o valor 23 por 24
+idade = 24;
+console.log(idade);
+
+const PI = 3.14; // declaração e atribuição de uma constante
+console.log(PI);
+
+// PI - 14; // isto vai gerar erro
+// console.log(PI)
+
+// Tipagem Estatica - Definição de um tipo a cada variavel (Java, C, C++, TypeScript)
+// Tipagem Dinamica - O tipo da variavel é definido de acordo com seu valor (JavaScript, Python)
+
+// let variavel = 90; Number
+// variavel = "Texto"; String
+// variavel = true; Boleano
