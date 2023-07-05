858ca174aba7fce26f4f43e265a886464a2b9441
Coder_Kanon <carloseduardomendes201@gmail.com>
2023-07-05 09:51:37 -0300

    Atualizado

---
+++ Aula-05-07-2023/variaveis-concatenacao.js
@@ -0,0 +1,25 @@
+// Escopo Global  (var x let)
+
+// let numero1 = 22;
+// console.log(numero1);
+
+// console.log(numero2);
+// var numero2 = 24;
+
+// Operador de Concatenação +
+const numero1 = 2 + 3; // Operador de Adição
+const texto = "O numero é " + numero1; //Operador de concatenação
+// console.log(texto);
+
+console.log("A soma de " + 2 + " com " + 3 + " é igual a " + numero1);
+
+let idade = 18;
+let mensagem = (idade >= 18) ? " Sua idade é " + idade + " pode ser preso! " : " Sua idade é" + idade + " vai chegar sua vez";
+console.log(mensagem);
+
+const  nome = "Carlos";
+console.log("Bem-vindo, " + nome)
+
+
+
+
